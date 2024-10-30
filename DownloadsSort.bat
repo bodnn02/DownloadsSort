bd1c10c7144d4fc3d53e8579f9bf0429a66789d1
bodnn02 <bodnn03@gmail.com>
2024-10-30 13:12:31 +0200

    Added default config, readme, other changes: 	new file:   .gitignore 	deleted:    DownloadsSort.bat 	deleted:    DownloadsSort.lnk 	modified:   DownloadsSort.py 	new file:   README.md 	deleted:    config.json 	new file:   requirements.txt

--- DownloadsSort.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Запуск Python скрипта
-python DownloadsSort.py
-
-REM Пауза, чтобы окно консоли не закрылось сразу после выполнения
-pause
--- DownloadsSort.py
+++ DownloadsSort.py
@@ -2,15 +2,57 @@ import os
 import json
 import shutil
 import hashlib
+from platformdirs import user_downloads_dir, user_documents_dir, user_music_dir, user_pictures_dir, user_videos_dir
+
+CONFIG_PATH = 'config.json'
+
+# Получаем системные пути
+DEFAULT_CONFIG = {
+    "downloads": os.path.normpath(user_downloads_dir()),
+    "images": {
+        "path": os.path.normpath(user_pictures_dir()),
+        "extensions": [".jpg", ".png", ".gif"]
+    },
+    "music": {
+        "path": os.path.normpath(user_music_dir()),
+        "extensions": [".mp3", ".wav", ".flac", ".weba"]
+    },
+    "videos": {
+        "path": os.path.normpath(user_videos_dir()),
+        "extensions": [".mp4", ".avi", ".mkv", ".mov"]
+    },
+    "executables": {
+        "path": os.path.normpath(os.path.join(user_downloads_dir(), "Executables")),
+        "extensions": [".exe", ".msi"]
+    },
+    "documents": {
+        "path": os.path.normpath(user_documents_dir()),
+        "extensions": [".docx", ".pdf", ".txt"]
+    },
+    "compressed": {
+        "path": os.path.normpath(os.path.join(user_downloads_dir(), "Compressed")),
+        "extensions": [".zip", ".7z", ".rar"]
+    },
+    "databases": {
+        "path": os.path.normpath(os.path.join(user_downloads_dir(), "Databases")),
+        "extensions": [".db", ".sqlite", ".sql"]
+    },
+    "torrents": {
+        "path": os.path.normpath(os.path.join(user_downloads_dir(), "Torrents")),
+        "extensions": [".torrent"]
+    }
+}
 
 def load_config():
+    if not os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, 'w', encoding='utf-8') as config_file:
+            json.dump(DEFAULT_CONFIG, config_file, ensure_ascii=False, indent=4)
+        print("Создан файл config.json с настройками по умолчанию.")
+        return DEFAULT_CONFIG
     try:
-        with open('config.json', 'r', encoding='utf-8') as config_file:
+        with open(CONFIG_PATH, 'r', encoding='utf-8') as config_file:
             config = json.load(config_file)
             return config
-    except FileNotFoundError:
-        print("Файл config.json не найден.")
-        return None
     except json.JSONDecodeError:
         print("Ошибка декодирования JSON в файле config.json.")
         return None
@@ -39,9 +81,7 @@ def move_files(source_folder, destination_folder, file_extensions):
                 destination_path = os.path.join(destination_folder, file_name)
 
                 if os.path.exists(destination_path):
-                    # Сравниваем хэши файлов
                     if get_file_hash(file_path) == get_file_hash(destination_path):
-                        # Если хэши одинаковы, переместим файл в корзину (можно использовать другую папку)
                         trash_folder = os.path.join(source_folder, "trash")
                         if not os.path.exists(trash_folder):
                             os.makedirs(trash_folder)
@@ -49,7 +89,6 @@ def move_files(source_folder, destination_folder, file_extensions):
                         print(f"Файл {file_name} уже существует в {destination_folder}. Перемещен в корзину.")
                         continue
                     else:
-                        # Если хэши разные, добавим число к имени файла
                         count = 1
                         new_file_name = os.path.splitext(file_name)[0] + f"_{count}" + os.path.splitext(file_name)[1]
                         while os.path.exists(os.path.join(destination_folder, new_file_name)):
@@ -65,11 +104,17 @@ def organize_downloads():
 
     if config is not None:
         downloads_folder = config.get("downloads", "")
-        if downloads_folder:
+        
+        if downloads_folder and os.path.exists(downloads_folder):
             for type_name, type_info in config.items():
                 if type_name != "downloads" and isinstance(type_info, dict):
                     file_extensions = type_info.get("extensions", [])
                     move_files(downloads_folder, type_info.get("path", ""), file_extensions)
+        else:
+            print("Папка для загрузок не найдена. Проверьте путь в config.json.")
+
 
 if __name__ == "__main__":
     organize_downloads()
+    print("Файлы успешно отсортированы.")
+    input("Нажмите Enter, чтобы закрыть...")
